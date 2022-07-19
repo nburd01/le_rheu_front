cfe910ac649b200df30a767b6327d7a31b249173
Niall Burdon <n.burdon@protonmail.com>
2022-07-19 18:06:32 +0200

    cleaning routes #1

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,7 +3,6 @@ import DisciplinesBar from '../../components/Disciplines/DisciplinesCards'
 import HeaderHome from '../../components/HeaderHome/HeaderHome'
 import LeClub from '../../components/LeClub/LeClub'
 import ScoreBar from '../../components/ScoreBar/ScoreBar'
-import UserList from '../../components/UserList/UserList'
 import Posts from '../../features/Post/Posts'
 
 export default function Home() {
@@ -11,10 +10,9 @@ export default function Home() {
     <div className="home">
         <HeaderHome/>
         <ScoreBar/>
-        <UserList/>
         <LeClub/>
         <DisciplinesBar/>
-        <Posts/>
+        <Posts/> 
     </div>
   )
 }
