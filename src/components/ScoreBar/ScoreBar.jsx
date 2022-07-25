bb0357f9b67dfbac4dbd10ab7e27f548e0420b63
Niall Burdon <n.burdon@protonmail.com>
2022-07-25 12:03:05 +0200

    addedscores to json fileand added them to scorebar

--- src/components/ScoreBar/ScoreBar.jsx
+++ src/components/ScoreBar/ScoreBar.jsx
@@ -6,14 +6,27 @@ import '../../assets/stylesheets/img.css';
 import '../../assets/stylesheets/font.css';
 import '../../assets/stylesheets/main.css';
 import '../../assets/stylesheets/tag.css';
+import Scores from  "../../json/scores.json";
 
 export default function ScoreBar() {
   return (
     <div className="score-bar">
     <div className="classements">
-        <h2>Classements</h2>
-        <h3>Le Rheu- Visiteur</h3>
-        <p>3 - 0</p>
+    <h2>Classements</h2>
+      <div>
+        {
+          Scores && Scores.map (
+            score => {
+              return(
+                <div>
+                  <h3>{score.locaux}  - {score.visiteurs}</h3>
+                  <p>{score.score_locaux} - {score.score_visiteurs}</p>
+                </div>
+              )
+            }
+          ) 
+        }
+      </div>
     </div>
     <div className="resultats">
         <h2>Résultats</h2>
