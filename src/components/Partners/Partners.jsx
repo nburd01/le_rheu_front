cc96a949487eab0c640d4e87518942bccaf80977
Niall Burdon <n.burdon@protonmail.com>
2022-08-31 19:44:49 +0200

    added partners, dimensioning to domore

---
+++ src/components/Partners/Partners.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import './Partners.css'
+
+export const Partners = () => {
+  return (
+    <div className="sponsorBlock">
+        <div className="__blockContent">
+            <img className='partnerLogo' src={require('../../assets/img/marques/blot.png')} alt={"logo le Rheu"}  />
+            <img className='partnerLogo' src={require('../../assets/img/marques/convivio.png')} alt={"logo le Rheu"}  />
+            <img className='partnerLogo' src={require('../../assets/img/marques/delArte.png')} alt={"logo le Rheu"}  />
+            <img className='partnerLogo' src={require('../../assets/img/marques/launay.png')} alt={"logo le Rheu"}  />
+            <img className='partnerLogo' src={require('../../assets/img/marques/puma.png')} alt={"logo le Rheu"}  />
+            <img className='partnerLogo' src={require('../../assets/img/marques/rennes.png')} alt={"logo le Rheu"}  />
+            <img className='partnerLogo' src={require('../../assets/img/marques/samsic.png')} alt={"logo le Rheu"}  />
+            <img className='partnerLogo' src={require('../../assets/img/marques/blot.png')} alt={"logo le Rheu"}  />
+            <img className='partnerLogo' src={require('../../assets/img/marques/convivio.png')} alt={"logo le Rheu"}  />
+            <img className='partnerLogo' src={require('../../assets/img/marques/delArte.png')} alt={"logo le Rheu"}  />
+            <img className='partnerLogo' src={require('../../assets/img/marques/launay.png')} alt={"logo le Rheu"}  />
+            <img className='partnerLogo' src={require('../../assets/img/marques/puma.png')} alt={"logo le Rheu"}  />
+            <img className='partnerLogo' src={require('../../assets/img/marques/rennes.png')} alt={"logo le Rheu"}  />
+            <img className='partnerLogo' src={require('../../assets/img/marques/samsic.png')} alt={"logo le Rheu"}  />
+            <img className='partnerLogo' src={require('../../assets/img/marques/blot.png')} alt={"logo le Rheu"}  />
+            <img className='partnerLogo' src={require('../../assets/img/marques/convivio.png')} alt={"logo le Rheu"}  />
+            <img className='partnerLogo' src={require('../../assets/img/marques/delArte.png')} alt={"logo le Rheu"}  />
+            <img className='partnerLogo' src={require('../../assets/img/marques/launay.png')} alt={"logo le Rheu"}  />
+            <img className='partnerLogo' src={require('../../assets/img/marques/puma.png')} alt={"logo le Rheu"}  />
+            <img className='partnerLogo' src={require('../../assets/img/marques/rennes.png')} alt={"logo le Rheu"}  />
+            <img className='partnerLogo' src={require('../../assets/img/marques/samsic.png')} alt={"logo le Rheu"}  />
+        </div>
+    </div>
+  )
+}
